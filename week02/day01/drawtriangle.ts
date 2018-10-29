fb030d5b68940dd6d7ca6b7a56db104b732cb617
Daniel Zsin <dani.zsinx@gmail.com>
2018-10-29 12:11:54 +0100

    drew a 4 row triangle with a for loop

---
+++ week02/day01/drawtriangle.ts
@@ -0,0 +1,17 @@
+'use strict';
+
+let lineCount: number = 4;
+
+// Write a program that draws a triangle like this:
+//
+// *
+// **
+// ***
+// ****
+//
+// The triangle should have as many lines as lineCount is
+let currentLine:string="";
+for( let i:number=0 ; i<lineCount ; i++){
+    currentLine=currentLine + "*";
+    console.log(currentLine);
+}
