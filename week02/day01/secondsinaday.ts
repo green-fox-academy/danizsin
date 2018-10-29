994b539d84d26ff21af824d8dfe94aa1df2738f2
Daniel Zsin <dani.zsinx@gmail.com>
2018-10-29 11:13:13 +0100

    calculated the remaining seconds of a day based on given values

---
+++ week02/day01/secondsinaday.ts
@@ -0,0 +1,11 @@
+'use strict';
+
+let currentHours: number = 14;
+let currentMinutes: number = 34;
+let currentSeconds: number = 42;
+
+// Write a program that prints the remaining seconds (as an integer) from a
+// day if the current time is represented by these variables
+let timenowinSecs:number = currentHours*3600+currentMinutes*60+currentSeconds;
+let secsinaDay:number = 24*3600;
+console.log(secsinaDay-timenowinSecs);
