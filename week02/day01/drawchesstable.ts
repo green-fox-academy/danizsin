7c2f856e2f7aed15b3d832b9658cf5082856d0ea
Daniel Zsin <dani.zsinx@gmail.com>
2018-10-29 15:53:16 +0100

    created a chess table with for loops and if statements

---
+++ week02/day01/drawchesstable.ts
@@ -0,0 +1,40 @@
+export { }
+'use strict';
+
+// Create a program that draws a chess table like this
+//
+// % % % %
+//  % % % %
+// % % % %
+//  % % % %
+// % % % %
+//  % % % %
+// % % % %
+//  % % % %
+//
+
+let sign: string = "#";
+let space: string = " ";
+let cols: number = 8;
+
+for (let i: number = 0; i < cols; i++) {
+    let currString: string = "";
+    if (i % 2 === 0) {
+        for (let j: number = 0; j < cols; j++) {
+            if (j % 2 === 0) {
+                currString = currString + sign;
+            } else {
+                currString = currString + space;
+            }
+        }
+    } else {
+        for (let l: number = 0; l < cols; l++) {
+            if (l % 2 === 0) {
+                currString = currString + space;
+            } else {
+                currString = currString + sign;
+            }
+        }
+    }
+    console.log(currString);
+}
