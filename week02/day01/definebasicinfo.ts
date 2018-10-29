d1aa2650ac3e34e1db9d0163148f44ac4f6bd06f
Daniel Zsin <dani.zsinx@gmail.com>
2018-10-29 10:33:17 +0100

    console logged my basic infos

---
+++ week02/day01/definebasicinfo.ts
@@ -0,0 +1,19 @@
+'use strict';
+
+let myName:string = "Dani";
+console.log(myName);
+
+let myAge:number = 22;
+console.log(myAge);
+
+let myHeight:number = 1.75;
+console.log(myHeight);
+
+let imMarried:boolean = false;
+console.log(imMarried);
+
+// Define several things as a variable, then print their values
+// Your name as a string
+// Your age as an integer
+// Your height in meters as a float
+// Whether you are married or not as a boolean
