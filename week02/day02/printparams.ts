a4cb5fecbb4bf4334b4c361e87ffb7848d84df27
Daniel Zsin <dani.zsinx@gmail.com>
2018-10-30 10:59:54 +0100

    created a function that prints all params given to it

---
+++ week02/day02/printparams.ts
@@ -0,0 +1,9 @@
+// -  Create a function called `printParams`
+//    which logs to the console the input parameters
+//    (can have multiple number of arguments)
+
+function printParams(...haha:any[]){
+    console.log(haha);
+}
+
+printParams(1,"haha", false, 2);
