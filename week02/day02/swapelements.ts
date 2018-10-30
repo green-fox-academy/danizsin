72644ef678913fa850814d826b8026c802b5bfd7
Daniel Zsin <dani.zsinx@gmail.com>
2018-10-30 11:14:53 +0100

    swapped the first and third elements of given array

---
+++ week02/day02/swapelements.ts
@@ -0,0 +1,8 @@
+// -  Create a variable named `abc` with the following content: `["Arthur", "Boe", "Chloe"]`
+// -  Swap the first and the third element of `abc`
+
+let abc: string[] = ["Arthur", "Boe", "Chole"];
+
+let rev: string[] = abc.reverse();
+
+console.log(rev);
