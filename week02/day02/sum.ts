cc2dc18282f00e2f2cec0245b61d420f9d309058
Daniel Zsin <dani.zsinx@gmail.com>
2018-10-30 10:35:25 +0100

    created a function which sums all numbers until the given par

---
+++ week02/day02/sum.ts
@@ -0,0 +1,12 @@
+// -  Write a function called `sum` that sum all the numbers until the given parameter
+// -  The function should return the result
+
+function sum(par:number){
+    let summ:number=0;
+    for(let i:number=0; i<=par; i++){
+        summ+=i;
+    }
+    return summ;
+}
+
+console.log(sum(4));
