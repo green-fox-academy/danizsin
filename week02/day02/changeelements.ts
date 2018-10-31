96eb16a2161ae08ad5d15d79843d3133274f93f4
Daniel Zsin <dani.zsinx@gmail.com>
2018-10-31 09:37:48 +0100

    added export where needed

--- week02/day02/changeelements.ts
+++ week02/day02/changeelements.ts
@@ -1,7 +1,7 @@
 // -  Create an array named `numList` with the following content: `[1, 2, 3, 8, 5, 6]`
 // -  Change the 8 to 4 with the `.map` method 
 // -  Print the fourth element as a test
-
+export{}
 let numList: number[] = [1, 2, 3, 8, 5, 6];
 
 let chan: number[] = numList.map(function (e) {
