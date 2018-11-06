3389f615a374f4fd58fb6846b37268242e7135d5
Daniel Zsin <dani.zsinx@gmail.com>
2018-11-06 11:10:45 +0100

    added a substring into an already existing string

---
+++ week03/day02/takeslonger.ts
@@ -0,0 +1,8 @@
+
+'use strict';
+// When saving this quote a disk error has occured. Please fix it.
+// Add "always takes longer than" to between the words "It" and "you"
+
+let quote: string = 'Hofstadter\'s Law: It you expect, even when you take into account Hofstadter\'s Law.'
+quote = quote.substring(0, 21) + 'always takes longer than' + quote.substring(20);
+console.log(quote);
