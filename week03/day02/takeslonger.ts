23561bffba5514e31de45faf68027a0123e4ef3f
Daniel Zsin <dani.zsinx@gmail.com>
2018-11-06 11:24:42 +0100

    added ; after quote variable

--- week03/day02/takeslonger.ts
+++ week03/day02/takeslonger.ts
@@ -3,6 +3,6 @@
 // When saving this quote a disk error has occured. Please fix it.
 // Add "always takes longer than" to between the words "It" and "you"
 
-let quote: string = 'Hofstadter\'s Law: It you expect, even when you take into account Hofstadter\'s Law.'
+let quote: string = 'Hofstadter\'s Law: It you expect, even when you take into account Hofstadter\'s Law.';
 quote = quote.substring(0, 21) + 'always takes longer than' + quote.substring(20);
 console.log(quote);
