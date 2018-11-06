4ff63e5da115dc48a97111b4fad73db6dd0461cd
Daniel Zsin <dani.zsinx@gmail.com>
2018-11-06 09:52:06 +0100

    changed indentation to right values

--- week03/day02/reverse.ts
+++ week03/day02/reverse.ts
@@ -6,11 +6,11 @@
 let reversed: string = '.eslaf eb t\'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI';
 
 function reverse(str) {
-    let reved = "";
-    for (let char of str) {
-        reved = char + reved;
-    }
-    return reved;
+	let reved = "";
+	for (let char of str) {
+		reved = char + reved;
+	}
+	return reved;
 }
 console.log(reverse(reversed));
 
