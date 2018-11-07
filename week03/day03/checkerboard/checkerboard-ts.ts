180b531f0c752756a8a1d5f4d631f67058365b76
Daniel Zsin <dani.zsinx@gmail.com>
2018-11-07 17:32:35 +0100

    made turbo boosts for checkerboard drawing, added input and alert

--- week03/day03/checkerboard/checkerboard-ts.ts
+++ week03/day03/checkerboard/checkerboard-ts.ts
@@ -8,32 +8,61 @@ const ctx = canvas.getContext('2d');
 
 // Fill the canvas with a checkerboard pattern.
 
-function drawChecker(x) {
-  for (let j: number = 0; j < canvas.height / x; j++) {
-    for (let i: number = 0; i < canvas.width / x; i++) {
-      if (j % 2 === 0) {
-        if (i % 2 === 0) {
-          ctx.fillStyle = 'black';
-          ctx.fillRect(i * x, j * x, x, x);
-        } else {
-          ctx.fillStyle = 'white';
-          ctx.fillRect(i * x, j * x, x, x);
-        }
-      } else {
-        if (i % 2 === 0) {
-          ctx.fillStyle = 'white';
-          ctx.fillRect(i * x, j * x, x, x);
-        } else {
-          ctx.fillStyle = 'black';
-          ctx.fillRect(i * x, j * x, x, x);
-        }
+
+function drawChecker(x: number, y: number, z: number) {
+  ctx.fillRect(x, y, z, z);
+}
+
+function makeTable(k: number) {
+  for (let j: number = 0; j < canvas.height / k; j++) {
+    for (let i: number = 0; i < canvas.width / k; i++) {
+      if ((i + j) % 2 === 0) {
+        drawChecker(i * k, j * k, k);
       }
     }
   }
 }
-document.getElementById('clickme').addEventListener('click', function () {
-  drawChecker(document.getElementById('num').value);
-  document.getElementById('num').value = '';
-  document.getElementById('num').focus();
 
+let clickbutton: any = document.getElementById('clickme');
+let inputval: any = document.getElementById('num');
+
+clickbutton.addEventListener('click', function () {
+  if (inputval.value.trim().length > 0) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    makeTable(+inputval.value);
+    inputval.value = '';
+    inputval.focus();
+  } else alert('Type a number, don\'t leave it empty!');
 });
+
+
+
+
+// function tralala(x) {
+//   for (let j: number = 0; j < canvas.height / x; j++) {
+//     for (let i: number = 0; i < canvas.width / x; i++) {
+//       if (j % 2 === 0) {
+//         if (i % 2 === 0) {
+
+//         } else {
+//           ctx.fillStyle = 'white';
+//           ctx.fillRect(i * x, j * x, x, x);
+//         }
+//       } else {
+//         if (i % 2 === 0) {
+//           ctx.fillStyle = 'white';
+//           ctx.fillRect(i * x, j * x, x, x);
+//         } else {
+//           ctx.fillStyle = 'black';
+//           ctx.fillRect(i * x, j * x, x, x);
+//         }
+//       }
+//     }
+//   }
+// }
+// document.getElementById('clickme').addEventListener('click', function () {
+//   tralala(document.getElementById('num').value);
+//   document.getElementById('num').value = '';
+//   document.getElementById('num').focus();
+
+// });
