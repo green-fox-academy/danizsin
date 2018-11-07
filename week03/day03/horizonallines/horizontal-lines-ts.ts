8c0bae50cd7d81f53a9d6668bfc835e4d86b90ec
Daniel Zsin <dani.zsinx@gmail.com>
2018-11-07 11:07:43 +0100

    drew 3 50long lines with loop

---
+++ week03/day03/horizonallines/horizontal-lines-ts.ts
@@ -0,0 +1,24 @@
+
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+
+// create a line drawing function that takes 2 parameters:
+// the x and y coordinates of the line's starting point
+// and draws a 50 long horizontal line from that point.
+// draw 3 lines with that function. Use loop for that.
+
+function drawLine(x, y) {
+  ctx.beginPath();
+  ctx.strokeStyle = 'orange';
+  ctx.moveTo(x, y);
+  ctx.lineTo(x, y+50);
+  ctx.stroke();
+}
+
+for (let i: number = 0; i <= 2; i++) {
+  drawLine(100+i*100, 100);
+}
