34a375f169f7ab63df7595512142547606da197a
Daniel Zsin <dani.zsinx@gmail.com>
2018-11-07 12:25:12 +0100

    drew 19 squares in a diagonal line

---
+++ week03/day03/purplesteps/purple-steps-ts.ts
@@ -0,0 +1,20 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+
+// Reproduce this:
+// [https://github.com/green-fox-academy/teaching-materials/blob/master/workshop/drawing/purple-steps/r3.png]
+
+function purpleSq(x, y) {
+  ctx.fillStyle = '#b145f3';
+  ctx.fillRect(x, y, 12, 12);
+  ctx.strokeStyle = 'black';
+  ctx.strokeRect(x, y, 12, 12);
+}
+
+for (let i: number = 1; i < 20; i++) {
+  purpleSq(12 * i, 12 * i);
+}
