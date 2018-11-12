d1a21c175321481f8f5108886526be96122878aa
Daniel Zsin <dani.zsinx@gmail.com>
2018-11-12 14:59:36 +0100

    feat(copyfile): added function to copy content of file to another

---
+++ week04/day01/copyfile.ts
@@ -0,0 +1,25 @@
+// Write a function that copies a file to an other
+// It should take the filenames as parameters
+// It should return a boolean that shows if the copy was successful
+
+export { };
+const fs = require('fs');
+
+function readFromFile(fileName: string): string {
+  try {
+    return fs.readFileSync(fileName, 'utf-8');
+  }
+  catch (e) {
+    console.log(`couldnt find this file: ${fileName}`);
+    return null;
+  }
+}
+
+function copyContent(fromFile: string, toFile: string) {
+  const myFile: string = readFromFile(fromFile);
+  if (myFile !== null) {
+    fs.appendFileSync(toFile, myFile);
+  }
+}
+
+copyContent('myfile.txt', 'newfile.txt');
