ba42864cfcad021652fd59c52d52f35ee8a01477
Daniel Zsin <dani.zsinx@gmail.com>
2018-11-12 14:51:00 +0100

    fix(countlines): added file reading function

--- week04/day01/countlines.ts
+++ week04/day01/countlines.ts
@@ -7,19 +7,22 @@
 export { };
 const fs = require('fs');
 
+function readFile(fileName: string): string {
+  try {
+    return fs.readFileSync(fileName, 'utf-8');
+  }
+  catch (e) {
+    return null;
+  }
+}
 
 function countEachline(fileName: string): number {
   let counter: number = 0;
-  try {
-    const myFile: string = fs.readFileSync(fileName, 'utf-8');
+  const myFile: string = readFile(fileName);
+  if (myFile !== null) {
     counter += myFile.split('\r\n').length;
     return counter;
-  }
-  catch (e) {
-    return counter;
-  }
-
+  } else return counter;
 }
 
 console.log(countEachline('myfile.txt'));
-// console.log(readFile('my-file.txt'));
