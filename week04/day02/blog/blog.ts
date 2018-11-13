a322976c528cc8286b33457b89d981abca40275b
Daniel Zsin <dani.zsinx@gmail.com>
2018-11-13 21:27:45 +0100

    add(blog): added one variable for the blogpost update

--- week04/day02/blog/blog.ts
+++ week04/day02/blog/blog.ts
@@ -32,6 +32,7 @@ blog1.addBlogPost(new BlogPost('John Doe', 'Lorem Ipsum', 'Lorem ipsum dolor sit
 blog1.addBlogPost(new BlogPost('Tim Urban', 'Wait but why', 'A popular long-form, stick-figure-illustrated blog about almost everything.', '2010.10.10.'));
 blog1.addBlogPost(new BlogPost('William Turton', 'One Engineer Is Trying to Get IBM to Reckon With Trump', 'Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention. When I asked to take his picture outside one of IBM’s New York City offices, he told me that he wasn’t really into the whole organizer profile thing.', '2017.03.28.'));
 
+let blogpost1 = new BlogPost('Dani Zsin', 'My life in nutshell', 'Pokemon theme song: \'I wanna be the very best like noone ever was, to catch them is my real test!\'', '2002.05.23');
 console.log(blog1.deleteBlogPost(2));
 console.log(blog1.deleteBlogPost(4));
-console.log(blog1.updateBlogPost(1, new BlogPost('Dani Zsin', 'My life in nutshell', 'Pokemon theme song: \'I wanna be the very best like noone ever was, to catch them is my real test!\'', '2002.05.23')));
+console.log(blog1.updateBlogPost(1, blogpost1));
