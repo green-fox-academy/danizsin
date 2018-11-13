bfde839df6f0ab4f9f1dba476f9385f88bd7118a
Daniel Zsin <dani.zsinx@gmail.com>
2018-11-13 11:11:59 +0100

    feat(sharpie): created Sharpie class and used the use() method

---
+++ week04/day02/sharpie.ts
@@ -0,0 +1,22 @@
+'use strict';
+export { };
+
+class Sharpie {
+  color: string;
+  width: number;
+  inkAmount: number;
+
+  constructor(clr: string, wdth: number, inkA: number = 100) {
+    this.color = clr;
+    this.width = wdth;
+    this.inkAmount = inkA;
+  }
+
+  use() {
+    return this.inkAmount - 1;
+  }
+}
+
+let sharpie1 = new Sharpie('blue', 200);
+
+console.log(sharpie1.use());
