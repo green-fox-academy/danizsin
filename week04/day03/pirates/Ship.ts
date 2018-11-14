9ccfaa257bfc8e6cb9cb0294c5e6221fc7aa6263
Daniel Zsin <dani.zsinx@gmail.com>
2018-11-14 15:15:33 +0100

    add(ship): added battle method

--- week04/day03/pirates/Ship.ts
+++ week04/day03/pirates/Ship.ts
@@ -19,17 +19,17 @@ export class Ship {
     this.crew.push(addCrew);
     this.crew.forEach(e => {
       e.forEach(elem => {
-        console.log(`${elem.name} is ready for duty!!!\r\n`);
+        // console.log(`${elem.name} is ready for duty!!!`);
       });
     });
-    console.log(`${this.captain.name} is the captain of the ship!!!`);
+    // console.log(`${this.captain.name} is the captain of the ship!!!\r\n\r\n`);
   }
 
   captainsDrunkLevel(): number {
     return this.captain.isDrunk;
   }
 
-  isCaptainDead(): boolean {
+  isCaptaisDead(): boolean {
     return this.captain.isDead;
   }
 
@@ -43,5 +43,60 @@ export class Ship {
       });
     });
     return countAlives;
+  }
+
+  battle(againstThisShip: Ship): boolean {
+    const ownBattleScore: number = this.numberOfAlivePirates() - this.captainsDrunkLevel();
+    console.log(ownBattleScore);
+    const enemyBattleScore: number = againstThisShip.numberOfAlivePirates() - againstThisShip.captainsDrunkLevel();
+    console.log(enemyBattleScore);
+    const randNumOfRums: number = Math.floor(Math.random() * 7);
+    const randNumOfDeaths: number = Math.floor(Math.random() * 4);
+    if (ownBattleScore > enemyBattleScore) {
+      this.crew[0].forEach(elem => {
+        for (let i: number = 0; i < randNumOfRums; i++) {
+          elem.drinkSomeRum();
+        }
+      });
+      for (let j: number = 0; j < randNumOfRums; j++) {
+        this.captain.drinkSomeRum();
+      }
+      if (againstThisShip.numberOfAlivePirates() > randNumOfDeaths) {
+        againstThisShip.crew[0].forEach(e => {
+          for (let g: number = 0; g < randNumOfDeaths; g++) {
+            e.die();
+          }
+        });
+      } else {
+        againstThisShip.crew[0].forEach(e => {
+          for (let g: number = 0; g < againstThisShip.numberOfAlivePirates(); g++) {
+            e.die();
+          }
+        });
+      }
+    } else {
+      againstThisShip.crew[0].forEach(elem => {
+        for (let i: number = 0; i < randNumOfRums; i++) {
+          elem.drinkSomeRum();
+        }
+      });
+      for (let j: number = 0; j < randNumOfRums; j++) {
+        againstThisShip.captain.drinkSomeRum();
+      }
+      if (this.numberOfAlivePirates() > randNumOfDeaths) {
+        this.crew[0].forEach(e => {
+          for (let g: number = 0; g < randNumOfDeaths; g++) {
+            e.die();
+          }
+        });
+      } else {
+        this.crew[0].forEach(e => {
+          for (let g: number = 0; g < this.numberOfAlivePirates(); g++) {
+            e.die();
+          }
+        });
+      }
+    }
+    return ownBattleScore > enemyBattleScore;
   }
 }
