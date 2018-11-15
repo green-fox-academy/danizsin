8b22d157f67cf05a213c057e0d89aeb9caf51c2a
Daniel Zsin <dani.zsinx@gmail.com>
2018-11-15 14:41:59 +0100

    fix(string): removed unnecessary variable

--- week04/day04/string1.ts
+++ week04/day04/string1.ts
@@ -4,9 +4,8 @@
 const baseString: string = 'x x x x x x x x erfre fre f ererf  x x x erfx x xrerx re';
 
 function changeAllXToY(convertThis: string) {
-  let newStringWithoutX: string = convertThis;
   if (convertThis.indexOf('x') === -1) {
-    return newStringWithoutX;
+    return convertThis;
   }
   return changeAllXToY(convertThis.replace('x', 'y'));
 }
--- week04/day04/string2.ts
+++ week04/day04/string2.ts
@@ -3,9 +3,8 @@ export { };
 const baseString: string = 'x x x x x x x x erfre fre f ererf  x x x erfx x xrerx re';
 
 function removeAllX(convertThis: string) {
-  let newStringWithoutX: string = convertThis;
   if (convertThis.indexOf('x') === -1) {
-    return newStringWithoutX;
+    return convertThis;
   }
   return removeAllX(convertThis.replace('x', ''));
 }
