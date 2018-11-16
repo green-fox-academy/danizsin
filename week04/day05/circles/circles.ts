c2d315736290977d045173e6a4f1f474c0f47c09
Daniel Zsin <dani.zsinx@gmail.com>
2018-11-16 10:46:08 +0100

    fix(circles):removed unnecessary colors

--- week04/day05/circles/circles.ts
+++ week04/day05/circles/circles.ts
@@ -3,11 +3,7 @@
 const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 
-ctx.fillStyle = 'white';
-ctx.fillRect(0, 0, 600, 600);
-
 function drawCircles(x: number, y: number, size: number, repeat: number) {
-  ctx.strokeStyle = 'black';
   ctx.beginPath();
   ctx.arc(x, y, size / 2, 0, Math.PI * 2);
   ctx.stroke();
