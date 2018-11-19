1c9a42839a8075b9cc23f9fa0e4edc4900aacf64
Daniel Zsin <dani.zsinx@gmail.com>
2018-11-19 23:36:43 +0100

    fix(aircraft): commented out the status method to better see the result of the fight

--- week05/day01/aircraftcarrier/TestAircrafts.ts
+++ week05/day01/aircraftcarrier/TestAircrafts.ts
@@ -2,8 +2,8 @@ import { Carrier } from "./Carrier";
 import { F16 } from "./F16";
 import { F35 } from "./F35";
 
-let myCarrier = new Carrier(5000, 150);
-let enemyCarrier = new Carrier(5000, 110);
+let myCarrier = new Carrier(5000, 10);
+let enemyCarrier = new Carrier(5000, 5);
 
 for (let i: number = 0; i < 10; i++) {
   myCarrier.addAircraft(new F16());
@@ -17,5 +17,5 @@ for (let j: number = 0; j < 6; j++) {
 myCarrier.fill();
 enemyCarrier.fill();
 
-myCarrier.getStatus();
+// myCarrier.getStatus();
 myCarrier.fight(enemyCarrier);
