4d0eeec40fd82ab94dcd8a961452c576756f880d
Daniel Zsin <dani.zsinx@gmail.com>
2018-11-19 15:32:25 +0100

    feat(garden): created garden class with waterGarden, plantsneedingwater and addPlant methods

---
+++ week05/day01/garden/Garden.ts
@@ -0,0 +1,44 @@
+import { Plant } from "./Plant";
+
+export class Garden {
+  crew: Plant[];
+  constructor() {
+    this.crew = [];
+  }
+
+  addPlant(initPlant: Plant): void {
+    this.crew.push(initPlant);
+  }
+
+  plantsNeedingWater(): number {
+    let numOfPlantsNeedingWater: number = 0;
+    this.crew.forEach(e => {
+      if (e.constructor.name === 'Flower') {
+        if (e.getWaterLevel() < 5) {
+          numOfPlantsNeedingWater += 1;
+        }
+      } else if (e.constructor.name === 'Tree') {
+        if (e.getWaterLevel() < 10) {
+          numOfPlantsNeedingWater += 1;
+        }
+      }
+    });
+    return numOfPlantsNeedingWater;
+  }
+
+  waterGarden(waterAmount: number): void {
+    console.log(`Watering with: ${waterAmount}`);
+    let waterByThisAmount: number = waterAmount / this.plantsNeedingWater();
+    this.crew.forEach(e => {
+      if (e.constructor.name === 'Flower') {
+        if (e.getWaterLevel() < 5) {
+          e.setWaterLevel(waterByThisAmount * 0.75);
+        }
+      } else if (e.constructor.name === 'Tree') {
+        if (e.getWaterLevel() < 10) {
+          e.setWaterLevel(waterByThisAmount * 0.4);
+        }
+      }
+    });
+  }
+}
