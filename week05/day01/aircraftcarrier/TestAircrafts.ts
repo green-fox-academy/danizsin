df0f5f1b6c109f535b06fa7c0132f71f57148f39
Daniel Zsin <dani.zsinx@gmail.com>
2018-11-19 22:59:38 +0100

    feat(aircraft): created test app for aircraft carrier battles

---
+++ week05/day01/aircraftcarrier/TestAircrafts.ts
@@ -0,0 +1,21 @@
+import { Carrier } from "./Carrier";
+import { F16 } from "./F16";
+import { F35 } from "./F35";
+
+let myCarrier = new Carrier(5000, 150);
+let enemyCarrier = new Carrier(5000, 110);
+
+for (let i: number = 0; i < 10; i++) {
+  myCarrier.addAircraft(new F16());
+  enemyCarrier.addAircraft(new F16());
+}
+for (let j: number = 0; j < 6; j++) {
+  myCarrier.addAircraft(new F35());
+  enemyCarrier.addAircraft(new F35());
+}
+
+myCarrier.fill();
+enemyCarrier.fill();
+
+myCarrier.getStatus();
+myCarrier.fight(enemyCarrier);
