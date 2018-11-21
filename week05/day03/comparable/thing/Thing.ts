502456dc4d52452e08a85cb965ac9e9ef048d18f
Daniel Zsin <dani.zsinx@gmail.com>
2018-11-21 10:22:11 +0100

    add(thing): add comparing method to thing class

---
+++ week05/day03/comparable/thing/Thing.ts
@@ -0,0 +1,50 @@
+import { Comparable } from "./Comparable";
+
+class Thing implements Comparable {
+  private _completed: boolean;
+  private _name: string;
+
+  constructor(_name: string) {
+    this._name = _name;
+    this._completed = false;
+  }
+
+  public complete() {
+    this._completed = true;
+  }
+
+  getName() {
+    return this._name;
+  }
+  getCompleted() {
+    return this._completed;
+  }
+
+  compareTo(other: Thing): number {
+    if (this.getCompleted() && other.getCompleted() === false) {
+      return -1;
+    }
+    if (this.getCompleted() === false && other.getCompleted()) {
+      return 1;
+    }
+    return 0;
+  }
+}
+let newArray: Thing[] = [];
+let thing1 = new Thing('kefe');
+let thing2 = new Thing('sátor');
+let thing3 = new Thing('létra');
+let thing4 = new Thing('labda');
+thing1.complete();
+newArray.push(thing1);
+newArray.push(thing2);
+newArray.push(thing3);
+thing4.complete();
+newArray.push(thing4);
+
+newArray.sort(function (a: Thing, b: Thing): number {
+  return a.compareTo(b);
+});
+console.log(newArray);
+
+
