11ba2d5887a1f19f2ef181f55e586d9067d87b80
Daniel Zsin <dani.zsinx@gmail.com>
2018-11-21 11:56:53 +0100

    add(printable): implement printable interface with printallfields method

--- week05/day03/comparable/dominoes/domino.ts
+++ week05/day03/comparable/dominoes/domino.ts
@@ -1,12 +1,17 @@
 import { Comparable } from "./Comparable";
+import { Printable } from "../../printallfields/printAllFields";
 
-class Domino implements Comparable {
+class Domino implements Comparable, Printable {
   values: number[];
   constructor(valueA: number, valueB: number) {
     this.values = [valueA, valueB];
   }
   compareTo(other: Domino): number {
     return this.values[0] - other.values[0];
+  }
+
+  printAllFields() {
+    return Object.keys(this);
   }
 }
 
