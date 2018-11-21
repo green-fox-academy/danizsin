aa7aa6e16a8621d999573739fd124d2e5cb42fea
Daniel Zsin <dani.zsinx@gmail.com>
2018-11-21 11:37:15 +0100

    add(zoo): add flyable interface to bird class

--- week05/day03/zoo/Bird.ts
+++ week05/day03/zoo/Bird.ts
@@ -1,11 +1,16 @@
 import { Animal } from "./Animal";
+import { Flyable } from "../flyable/Flyable";
 
-export class Bird extends Animal {
+export class Bird extends Animal implements Flyable {
   constructor(name: string) {
     super(name);
   }
 
   breed() {
     return 'laying eggs.';
+  }
+
+  fly() {
+    return 'the bird can fly'
   }
 }
