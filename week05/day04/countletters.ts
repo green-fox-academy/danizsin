4e37400c2ef561513e43a64a1dd6ba740d6efa5f
Daniel Zsin <dani.zsinx@gmail.com>
2018-11-22 10:46:25 +0100

    fix(countletters): fix testing function change for deepEqual

--- week05/day04/countletters.ts
+++ week05/day04/countletters.ts
@@ -11,11 +11,11 @@ function countLetters(countInThis: string): any {
 }
 
 test('count the occurences of letters in a string', t => {
-  t.equal(countLetters('almafa'), { a: 3, l: 1, m: 1, f: 1 });
+  t.deepEqual(countLetters('almafa'), { a: 3, l: 1, m: 1, f: 1 });
   t.end();
 });
 
 test('count the occurences of letters in a string', t => {
-  t.equal(countLetters('szoborcsoport a Tanyan'), { s: 2, z: 1, o: 1, b: 4, r: 2, c: 1, p: 1, t: 1, a: 3, T: 1, n: 2, y: 1 });
+  t.deepEqual(countLetters('szoborcsoport a Tanyan'), { s: 2, z: 1, o: 4, b: 1, r: 2, c: 1, p: 1, t: 1, a: 3, T: 1, n: 2, y: 1 });
   t.end();
 });
