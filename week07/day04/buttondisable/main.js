5a94f77be8dac63cef5d7a7642ca024db88eff20
Daniel Zsin <dani.zsinx@gmail.com>
2018-12-13 23:12:49 +0100

    turbo booosts to buttondisable task

--- week07/day04/buttondisable/main.js
+++ week07/day04/buttondisable/main.js
@@ -5,34 +5,58 @@ const signupbtn = document.querySelector('.signup');
 const lovecatsbtn = document.querySelector('.lovecats');
 const buttons = document.querySelectorAll('button');
 const lilfish = document.querySelector('i');
-const doggy = document.querySelector('i#dog');
+const doggy = document.querySelector('i#doggy');
 
+const removeActiveClass = (btn) => {
+  btn.classList.remove('activebtn');
+  btn.classList.add('disabledbtn');
+  btn.innerHTML = 'DISABLED';
+}
+
+const addActiveClass = (btn) => {
+  btn.classList.remove('disabledbtn');
+  btn.classList.add('activebtn');
+  if (btn === signupbtn) {
+    btn.innerHTML = 'SIGN UP';
+  } else {
+    btn.innerHTML = 'I LOVE CATS';
+  }
+}
+
+removeActiveClass(signupbtn);
+removeActiveClass(lovecatsbtn);
 
 const barkDog = () => {
   const audio = document.querySelector('audio');
   audio.play();
 }
 
+
 form.addEventListener('change', () => {
   if (animal.value === 'dog' || animal.value === 'cat' || (animal.value === 'fish' && facts.value === 'nope')) {
     signupbtn.removeAttribute('disabled');
+    addActiveClass(signupbtn);
     if (animal.value === 'dog') {
       doggy.classList.add('animateme');
       barkDog();
     }
-  } else if (animal.value === 'fish' && (facts.value === 'yes' || facts.value === 'nope' || facts.value === '')) {
+  } else if (animal.value === 'fish') {
     if (facts.value != 'nope') {
       signupbtn.disabled = 'true';
+      removeActiveClass(signupbtn);
     }
     lilfish.classList.add('animateme');
   } else {
     signupbtn.disabled = 'true';
+    removeActiveClass(signupbtn);
   }
 
   if (facts.value === 'yes') {
     lovecatsbtn.removeAttribute('disabled');
+    addActiveClass(lovecatsbtn);
   } else {
     lovecatsbtn.disabled = 'true';
+    removeActiveClass(lovecatsbtn);
   }
 });
 
