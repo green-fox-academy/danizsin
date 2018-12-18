57f1e0172c2e0146a9ae9d2b493050019b98ddd8
Daniel Zsin <dani.zsinx@gmail.com>
2018-12-18 16:33:20 +0100

    removed todo.sql

--- week08/day02/todo.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE IF NOT EXISTS todoapp;
-
-CREATE TABLE IF NOT EXISTS todos (
-  'todoid' INT(9) NOT NULL AUTO_INCREMENT,
-  'todo' VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_hungarian_ci NOT NULL,
-  PRIMARY KEY('todoid')
-);
-
-INSERT INTO todos (todoid, todo) VALUES (1, "Baking"), (2, "Washing the dishes");
