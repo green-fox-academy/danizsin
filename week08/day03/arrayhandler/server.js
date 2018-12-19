d4486fad04809a7c736a1f263cae2b82dc66fac5
Daniel Zsin <dani.zsinx@gmail.com>
2018-12-19 14:23:54 +0100

    array handler task completed

---
+++ week08/day03/arrayhandler/server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bp = require('body-parser');
+
+const app = express();
+const PORT = 3000;
+
+app.use(bp());
+
+app.post('/arrays', (req, res) => {
+  const { what, numbers } = req.body;
+  switch (what) {
+    case 'sum':
+      let summ = 0;
+      numbers.forEach(e => {
+        summ += e;
+      });
+      res.json({ 'result': `${summ}` });
+      break;
+
+    case 'multiply':
+      let multiplication = 1;
+      numbers.forEach(e => {
+        multiplication *= e;
+      });
+      res.json({ 'result': `${multiplication}` });
+      break;
+
+    case 'double':
+      const doubled = numbers.map(e => e * 2);
+      res.json({ 'result': `${doubled}` });
+      break;
+
+    default: res.json({ 'error': 'Please provide what to do with the numbers!' })
+  }
+});
+
+app.listen(PORT);
